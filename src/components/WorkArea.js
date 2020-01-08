ad205b97e724d306a10817c6fa380f9235be69ce
Ol2iGiN <pongthep.dk@gmail.com>
2020-01-08 21:43:43 +0700

    push asearcher banner

--- src/components/WorkArea.js
+++ src/components/WorkArea.js
@@ -22,19 +22,19 @@ class WorkArea extends Component {
                                     <WorkAreaItem project_image_path="img/work/banner-asearcher.png" project_title="aSearcher" project_type="Client Project" />
                                 </div>
                                 <div className="single-work col-lg-4 col-md-6 col-sm-12 all web-design branding wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.2s">
-                                    <WorkAreaItem project_image_path="img/work/w2.jpg" project_title="2D Vinyl Design" project_type="Client Project" />
+                                    <WorkAreaItem project_image_path="img/work/banner-asearcher.png" project_title="aSearcher" project_type="Client Project" />
                                 </div>
                                 <div className="single-work col-lg-4 col-md-6 col-sm-12 all branding web-design wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.4s">
-                                    <WorkAreaItem project_image_path="img/work/w3.jpg" project_title="2D Vinyl Design" project_type="Client Project" />
+                                    <WorkAreaItem project_image_path="img/work/banner-asearcher.png" project_title="aSearcher" project_type="Client Project" />
                                 </div>
                                 <div className="single-work col-lg-4 col-md-6 col-sm-12 all web-design wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.6s">
-                                    <WorkAreaItem project_image_path="img/work/w6.jpg" project_title="2D Vinyl Design" project_type="Client Project" />
+                                    <WorkAreaItem project_image_path="img/work/banner-asearcher.png" project_title="aSearcher" project_type="Client Project" />
                                 </div>
                                 <div className="single-work col-lg-4 col-md-6 col-sm-12 all creative wow fadeInUp" data-wow-duration="2s">
-                                    <WorkAreaItem project_image_path="img/work/w4.jpg" project_title="2D Vinyl Design" project_type="Client Project" />
+                                    <WorkAreaItem project_image_path="img/work/banner-asearcher.png" project_title="aSearcher" project_type="Client Project" />
                                 </div>
                                 <div className="single-work col-lg-4 col-md-6 col-sm-12 all branding wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.2s">
-                                    <WorkAreaItem project_image_path="img/work/w5.jpg" project_title="2D Vinyl Design" project_type="Client Project" />
+                                    <WorkAreaItem project_image_path="img/work/banner-asearcher.png" project_title="aSearcher" project_type="Client Project" />
                                 </div>
                             </div>
                         </div>
